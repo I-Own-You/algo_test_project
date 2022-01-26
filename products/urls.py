9f6d3b4c0ff3ad75bfe3cb510f264886cc17ab37
your Master <91146968+I-Own-You@users.noreply.github.com>
2022-01-26 20:14:42 +0200

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,19 +2,7 @@ from django.urls import path, re_path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.schemas import get_schema_view
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
 from django.views.generic import TemplateView
-
-
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="Test Project",
-#       default_version='v1',
-#       description="Test description",
-#    ),
-#    public=True,
-# )
 
 
 urlpatterns = format_suffix_patterns([
@@ -33,8 +21,6 @@ urlpatterns = format_suffix_patterns([
     path('car-images/<int:pk>/', views.images_detail, name='images-detail'),
 
     path('openapi/', get_schema_view(), name='openapi-schema'),
-
-    # re_path(r'^swagger-ui/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     path('swagger-ui/', TemplateView.as_view(
         template_name='swagger-ui.html',
